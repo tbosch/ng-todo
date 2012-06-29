0b7e4e96c61b9ac505255a1bad057b95842d4c7f
Igor Minar <iiminar@gmail.com>
2012-06-28 17:28:54 -0700

    use spaces

--- local-server.js
+++ local-server.js
@@ -25,119 +25,121 @@ var nextUid = function() {
 }
 
 var DbStore = function() {
-	var store = [];
-	var map = {};
+  var store = [];
+  var map = {};
 
-	this.create = function(data) {
-		data._id = {
-			$oid: nextUid()
-		};
+  this.create = function(data) {
+    data._id = {
+      $oid: nextUid()
+    };
 
-		store.push(data);
-		map[data._id.$oid] = data;
-		return data;
-	};
+    store.push(data);
+    map[data._id.$oid] = data;
+    return data;
+  };
 
-	this.getAll = function() {
-		return store;
-	};
+  this.getAll = function() {
+    return store;
+  };
 
-	this.remove = function(id) {
-		var item = map[id];
+  this.remove = function(id) {
+    var item = map[id];
 
-		if (!item) return false;
+    if (!item) return false;
 
-		delete map[id];
-		store.splice(store.indexOf(item), 1);
-		return true;
-	};
+    delete map[id];
+    store.splice(store.indexOf(item), 1);
+    return true;
+  };
 
-	this.update = function(id, data) {
-		var item = map[id];
+  this.update = function(id, data) {
+    var item = map[id];
 
-		if (!item) return false;
+    if (!item) return false;
 
-		for (var key in data) {
-			if (data.hasOwnProperty(key)) {
-				item[key] = data[key];
-			}
-		}
-		
-		return item;
-	};
+    for (var key in data) {
+      if (data.hasOwnProperty(key)) {
+        item[key] = data[key];
+      }
+    }
+
+    return item;
+  };
 };
 
 var db = new DbStore();
-db.create({text: 'Try AngularJs', done: false});
-db.create({text: 'Visit Boston', done: true});
-db.create({text: 'Drink some beers', done: false});
+db.create({text: 'Try AngularJS', done: false});
+db.create({text: 'Visit Santa Monica', done: true});
+db.create({text: 'Have FUN!', done: false});
 
 var handler = {
 
-	GET: function(req, resp) {
-		resp.writeHead(200);
-		resp.write(JSON.stringify(db.getAll()));
-		resp.end();
-	},
+  GET: function(req, resp) {
+    resp.writeHead(200);
+    resp.write(JSON.stringify(db.getAll()));
+    resp.end();
+  },
 
-	PUT: function(req, resp, id) {
-		var data = '';
-		req.on('data', function(buffer) {
-			data += buffer.toString();
-		});
+  PUT: function(req, resp, id) {
+    var data = '';
+    req.on('data', function(buffer) {
+      data += buffer.toString();
+    });
 
-		req.on('end', function() {
-			var item = db.update(id, JSON.parse(data));
+    req.on('end', function() {
+      var item = db.update(id, JSON.parse(data));
 
-			if (item) {
-				resp.writeHead(200);
-				resp.write(JSON.stringify(item));
-			} else {
-				resp.writeHead(404);
-			}
-			resp.end();
-		});
-	},
+      if (item) {
+        resp.writeHead(200);
+        resp.write(JSON.stringify(item));
+      } else {
+        resp.writeHead(404);
+      }
+      resp.end();
+    });
+  },
 
-	POST: function(req, resp) {
-		var data = '';
-		req.on('data', function(buffer) {
-			data += buffer.toString();
-		});
+  POST: function(req, resp) {
+    var data = '';
+    req.on('data', function(buffer) {
+      data += buffer.toString();
+    });
 
-		req.on('end', function() {
-			resp.writeHead(201);
-			var item = db.create(JSON.parse(data));
-			resp.write(JSON.stringify(item));
-			resp.end();
-		});
-		
-	},
+    req.on('end', function() {
+      resp.writeHead(201);
+      var item = db.create(JSON.parse(data));
+      resp.write(JSON.stringify(item));
+      resp.end();
+    });
 
-	DELETE: function(req, resp, id) {
-		resp.writeHead(db.remove(id) ? 200 : 404);
-		resp.end();
-	},
+  },
 
-	OPTIONS: function(req, resp) {
-		resp.writeHead(200, {
-			'Access-Control-Allow-Headers': 'origin, x-requested-with, accept, content-type',
-			'Access-Control-Allow-Methods': ['POST', 'GET', 'OPTIONS', 'PUT', 'DELETE'],
-			'Access-Control-Max-Age': '1728000',
-			'Allow': ['POST', 'GET', 'OPTIONS', 'PUT', 'DELETE']
-		});
-		resp.write(JSON.stringify(db.getAll()));
-		resp.end();
-	}
+  DELETE: function(req, resp, id) {
+    resp.writeHead(db.remove(id) ? 200 : 404);
+    resp.end();
+  },
+
+  OPTIONS: function(req, resp) {
+    resp.writeHead(200, {
+      'Access-Control-Allow-Headers': 'origin, x-requested-with, accept, content-type',
+      'Access-Control-Allow-Methods': ['POST', 'GET', 'OPTIONS', 'PUT', 'DELETE'],
+      'Access-Control-Max-Age': '1728000',
+      'Allow': ['POST', 'GET', 'OPTIONS', 'PUT', 'DELETE']
+    });
+    resp.write(JSON.stringify(db.getAll()));
+    resp.end();
+  }
 };
 
 http.createServer(function(request, response) {
-	// CORS
-	response.setHeader('Access-Control-Allow-Credentials', 'true');
-	response.setHeader('Access-Control-Allow-Origin', '*');
+  console.log(request.url)
 
-	var id = /items\/([a-z0-9]*)/.exec(request.url);
-	handler[request.method](request, response, id && id[1]);
+  // CORS
+  response.setHeader('Access-Control-Allow-Credentials', 'true');
+  response.setHeader('Access-Control-Allow-Origin', '*');
+
+  var id = /items\/([a-z0-9]*)/.exec(request.url);
+  handler[request.method](request, response, id && id[1]);
 }).listen(80);
 
 
