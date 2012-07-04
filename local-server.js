7332111d7cb771e643b86b976fe129d9c8d39d74
Igor Minar <iiminar@gmail.com>
2012-07-03 17:19:23 -0700

    before persistence

--- local-server.js
+++ local-server.js
@@ -73,7 +73,6 @@ db.create({text: 'Help Brad write ToDo App', done: true, minion: 'Miško'});
 db.create({text: 'Rock RuPy', done: false, minion: 'Brad'});
 db.create({text: 'Take over the World', done: true, minion: 'Miško'});
 
-
 var handler = {
 
   GET: function(req, resp) {
