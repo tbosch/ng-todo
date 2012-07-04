47893c0445d77a0504bbdb392c093ad6bf801de2
Igor Minar <iiminar@gmail.com>
2012-07-03 17:49:47 -0700

    add persistence

--- todo.js
+++ todo.js
@@ -1,19 +1,15 @@
 var todoApp = angular.module('todoApp', ['ngResource']);
 
-todoApp.controller('AppCtrl', function AppCtrl($scope) {
+todoApp.controller('AppCtrl', function AppCtrl($scope, Item) {
 
   // define userName model
   $scope.userName = 'Igor';
 
-
-  // define items model
-  var items = [
-    {text: 'Make AngularJS Awesome', done: false, minion: 'Miško'},
-    {text: 'Help Brad write ToDo App', done: true, minion: 'Brad'}
-  ];
+  // fetch items model
+  var items = Item.query();
 
 
-  // publish it on scope
+  // publish items model
   $scope.items = items;
 
 
@@ -27,20 +23,23 @@ todoApp.controller('AppCtrl', function AppCtrl($scope) {
 
   // event handler
   $scope.add = function(newItem) {
-    var item = {
-      text: newItem.text,
-      minion: newItem.minion,
-      done: false
-    };
+    var item = new Item({text: newItem.text, done:false, minion:newItem.minion});
     items.push(item);
     newItem.text = '';
+
+    // save to mongolab
+    item.$save();
   };
 
 
   // event handler
   $scope.archive = function() {
     items = $scope.items = items.filter(function(item) {
-      return !item.done;
+      if (item.done) {
+        item.$remove();
+        return false;
+      }
+      return true;
     });
   };
 });
@@ -65,4 +64,28 @@ todoApp.filter('avatarUrl', function() {
   return function(userName) {
     return 'img/' + userName.toLowerCase() + '.png';
   }
+});
+
+
+todoApp.constant('apiKey', '4fc27c99e4b0401bdbfd1741');
+
+
+todoApp.factory('Item', function($resource, apiKey) {
+  var Item = $resource('http://offline.api.mongolab.com/api/1/databases/ng-todo/collections/items/:id', {
+    apiKey: apiKey
+  }, {
+    update: {method: 'PUT'}
+  });
+
+  Item.prototype.$remove = function() {
+    Item.remove({id: this._id.$oid});
+  };
+
+  Item.prototype.$update = function() {
+    return Item.update({id: this._id.$oid}, angular.extend({}, this, {_id: undefined}));
+  };
+
+  Item.prototype.done = false;
+
+  return Item;
 });
